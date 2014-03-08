92cb9b5673045198f1679f0ddd886e5c62093310
Chokwanghwan <kwang8992@gmail.com>
2014-03-08 21:29:02 +0900

    *타임라인 댓글 저장 및 화면 전환 기능 추가하기

--- timeline/timeline/app/controllers/wall_controller.rb
+++ timeline/timeline/app/controllers/wall_controller.rb
@@ -41,8 +41,18 @@ class WallController < ApplicationController
     redirect_to "/wall/posts"
   end
 
-  def write_commnet
+  def write_comment
     @post_comment = Post.find(params[:id])
+  end
+
+  def write_comment_complete
+    c = Comment.new
+    c.post_id = params[:post_id]
+    c.name = params[:writer]
+    c.content = params[:content]
+    c.save
+
+    redirect_to "/wall/posts"
   end
 
   def posts
