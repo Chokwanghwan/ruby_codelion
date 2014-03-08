c3c6f1a3d01da746cec19768c26b1c62a8d8a761
Chokwanghwan <kwang8992@gmail.com>
2014-03-08 20:48:34 +0900

    *마이그레이션 파일과 모델 파일 작성하고 댓글 테이블 만들기

--- timeline/timeline/db/schema.rb
+++ timeline/timeline/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308041608) do
+ActiveRecord::Schema.define(version: 20140308114542) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "post_id"
+    t.string   "name"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "name"
