2bb184204d576cb34c90ddfbc1da5fd003fb15a3
Chokwanghwan <kwang8992@gmail.com>
2014-03-17 23:49:11 +0900

    *게시글 리스트 상에 디자인 적용하기

---
+++ gourmet/db/migrate/20140316160939_add_image_to.rb
@@ -0,0 +1,4 @@
+class AddImageTo < ActiveRecord::Migration
+  def change
+  end
+end
--- gourmet/db/schema.rb
+++ gourmet/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140316135124) do
+ActiveRecord::Schema.define(version: 20140316160939) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
